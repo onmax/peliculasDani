fc70a2118fe6c1a88cf710915c985697e6e1ee93
Máximo J. García Martínez <maximo.garcia.martinez@alumnos.upm.es>
2017-04-10 15:01:26 +0200

    Errores y nuevas características
    Se han añadido el footer, parametros de búsqueda, login entre otras
    cosas.
    Multitud de errores corregidos.

--- slider.js
+++ slider.js
@@ -8,26 +8,38 @@ var ancho_real = $(window).width() * 0.8
 var aux = 0;
 var limite = 0;
 
+var id_prev = document.getElementById("prev");
+var id_next = document.getElementById("next");
 
+var tope = 0;
 next.addEventListener("click", function next(){
   aux -= ancho;
-  if(aux - ancho >=carteles.width()*(-1)){
+  limite++;
+  if(aux - ancho >= carteles.width()*(-1) && tope != limite){
     sliderContent.style.transform = 'translateX(' + aux + 'px)';
+    id_next.style.visibility = "visible";
+    id_prev.style.visibility = "visible";
   }else{
+    tope = limite;
     aux = carteles.width()*(-1) + ancho_real - 10;
     sliderContent.style.transform = 'translateX(' + aux + 'px)';
+    id_next.style.visibility = "hidden";
+    id_prev.style.visibility = "visible";
   }
-  limite++;
 });
 
 prev.addEventListener("click", function prev(){
   if(limite>0){
     aux+= ancho;
     if(limite == 1){
-      aux =0 ;  
+      aux = 0;
       sliderContent.style.transform = 'translateX(' + 0 + 'px)';
+      id_prev.style.visibility = "hidden"
+      id_next.style.visibility = "visible";
     }else{
       sliderContent.style.transform = 'translateX(' + aux + 'px)';
+      id_prev.style.visibility = "visible";
+      id_next.style.visibility = "visible";
     }
     limite--;
   }
--- sticky_menu.js
+++ sticky_menu.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
 	var altura = $('.header').offset().top;
 
 	$(window).on('scroll', function(){
-		if ($(window).scrollTop() > altura){
+		if ($(window).scrollTop() > altura + 16){
 			$('.header').addClass('search_fixed');
       $('.container').addClass('container_fixed');
-
+			$('.login').addClass('login_fixed');
+			$('.parametros').addClass('parametros_fixed');
 		} else {
 			$('.header').removeClass('search_fixed');
-      $('.container').removeClass('container_fixed')
+      $('.container').removeClass('container_fixed');
+			$('.login').removeClass('login_fixed');
+			$('.parametros').removeClass('parametros_fixed');
 		}
 	});
 
